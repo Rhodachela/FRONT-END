ae2e2146845ca39709410e66131bdd43e4efc097
Rhoda <rhodachela@gmail.com>
2025-10-28 14:17:51 +0300

    Javascript for accordion

---
+++ JS-Projects/Accordion/app.js
@@ -0,0 +1,10 @@
+const accordion = document.getElementsByClassName('content-container');
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function(){
+    for (let j = 0; j < accordion.length; j++){
+      if (i != j) accordion[j].classList.remove('active');
+    }
+    this.classList.toggle('active');
+  });
+};
